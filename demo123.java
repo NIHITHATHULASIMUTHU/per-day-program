305d6011ccb816c9e4f0a62a64c9a274cbcb1ad4
NIHITHATHULASIMUTHU <nihitha0426@gmail.com>
2026-03-19 12:08:16 +0530

    Add files via upload

---
+++ demo123.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class demo123
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();
+		String s="";
+		int j=0;
+		while (n>0)
+		{
+			j=n%10;
+			s=s+j;
+			n/=10;
+		}
+		System.out.println(s);
+		if ()
+		{
+			for (; ; )
+			{
+			}
+		}
+	}
+}
